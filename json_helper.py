3206fe2e67a3172ec5228340fd19836e76fceeb1
jwheller0013 <jwheller0013@yahoo.com>
2025-03-20 16:48:08 -0400

    Part B complete

--- json_helper.py
+++ json_helper.py
@@ -1,12 +1,24 @@
 import json
+import os
+
 
 def read_json (file_name):
     with open(file_name, 'r') as file:
         data = json.load(file)
-        return data
+    return data
+
+def read_all_json (file_path):
+    all_list = []
+    for file in os.listdir(file_path):
+        new_file_path = "%s/%s" % (file_path, file)
+        data = read_json(new_file_path)
+        all_list.append(data)
+    return all_list
+
 
 
         # for key, value in data.items():
         #     print(f"{key}: {value}")
 
-read_json('/Users/jim/Projects/p1/PythonFundamentals.Exercises.Part9/data/super_smash_bros/link.json')
+read_all_json('/Users/jim/Projects/p1/PythonFundamentals.Exercises.Part9/data/super_smash_bros/')
+
