45d99c4691d382e645fd3a35742c6b24a98249ee
jwheller0013 <jwheller0013@yahoo.com>
2025-03-22 19:50:45 -0400

    Had to flip dicts for read all test to work

--- test_json_helper.py
+++ test_json_helper.py
@@ -7,25 +7,31 @@ class JsonHelperTester(unittest.TestCase):
     def setUp(self) -> None:
         self._json_stubs = [
             {
-                "name": "Link",
-                "neutral_special": "Bow and Arrows",
-                "side_special": " Boomerang",
-                "up_special": " Spin Attack",
-                "down_special": "Remote Bomb",
-                "final_smash": "Ancient Bow and Arrow"
-            },
-            {
                 "name": "Mario",
                 "neutral_special": "Fireball",
                 "side_special": "Cape",
                 "up_special": "Super Jump Punch",
                 "down_special": "F.L.U.D.D.",
                 "final_smash": "Mario Finale"
+            },
+            {
+                "name": "Link",
+                "neutral_special": "Bow and Arrows",
+                "side_special": " Boomerang",
+                "up_special": " Spin Attack",
+                "down_special": "Remote Bomb",
+                "final_smash": "Ancient Bow and Arrow"
             }
         ]
 
     def test_read_json(self):
-        expected = self._json_stubs[0]
+        expected = self._json_stubs[1]
         file_path = os.path.join('./', 'data', 'super_smash_bros', 'link.json')
         actual = json_helper.read_json(file_path)
+        self.assertEqual(expected,actual)
+
+    def test_read_all_json(self):
+        expected = self._json_stubs
+        file_path = os.path.join('./', 'data', 'super_smash_bros')
+        actual = json_helper.read_all_json(file_path)
         self.assertEqual(expected,actual)
