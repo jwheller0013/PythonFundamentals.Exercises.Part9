628f26b082e923efbeafc34058a095dcbefd0465
jwheller0013 <jwheller0013@yahoo.com>
2025-03-22 19:24:03 -0400

    Pickles runs but need to test

--- json_helper.py
+++ json_helper.py
@@ -15,10 +15,18 @@ def read_all_json (file_path):
         all_list.append(data)
     return all_list
 
+def write_pickle(file_path, data):
+    json_object = json.dumps(data)
+    with open("super_smash_characters.pickle", mode = "w") as outfile:
+        outfile.write(json_object)
 
+def load_pickle(file_path):
+    with open(file_path, 'r') as file:
+        pickle = json.load(file)
+    print(pickle)
 
         # for key, value in data.items():
         #     print(f"{key}: {value}")
 
-read_all_json('/Users/jim/Projects/p1/PythonFundamentals.Exercises.Part9/data/super_smash_bros/')
+# read_all_json('/Users/jim/Projects/p1/PythonFundamentals.Exercises.Part9/data/super_smash_bros/')
 
